e5973d4b80f812c8e770ca07b48b523fda368874
raosay <raosay@users.noreply.github.com>
2015-09-08 13:31:49 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,7 +3,7 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "raosay";
+	public static final String TOKEN = "123456";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
 	public static final String APIKEY = "ec6fb16e97c4029d99c4a1d8f0cdd870"; 
